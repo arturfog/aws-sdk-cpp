974d2ed85991be3768ab9a17517b4f6f4a42a5db
Artur <arturprojekty1988@gmail.com>
2020-11-05 21:34:37 +0100

    Update compiler_settings.cmake
    Removed waning as error default

--- cmake/compiler_settings.cmake
+++ cmake/compiler_settings.cmake
@@ -149,16 +149,5 @@ macro(set_msvc_warnings)
             set(CMAKE_SHARED_LINKER_FLAGS_DEBUGOPT "")
         endif()
 
-        # warnings as errors, max warning level (4)
-        if(NOT CMAKE_CXX_FLAGS MATCHES "/WX")
-            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /WX")
-        endif()
-
-        # taken from http://stackoverflow.com/questions/2368811/how-to-set-warning-level-in-cmake
-        if(CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
-            string(REGEX REPLACE "/W[0-4]" "/W4" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-        else()
-            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W4")
-        endif()
     endif(MSVC)
 endmacro()
